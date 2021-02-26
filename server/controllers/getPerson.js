7ca3281fe835c3bd8bbe775f756fcaf8643a2f16
YousefQwasmeh <151037@ppu.edu.ps>
2021-02-26 15:18:23 +0200

    console log

--- server/controllers/getPerson.js
+++ server/controllers/getPerson.js
@@ -2,7 +2,7 @@ const getPersonQuery = require("../database/queries/getPerson");
 
 const getPerson = (req, res) => {
   console.log(req.url, "666666666666666666");
-
+  res.send([{ id: 17, name: "احمد" }]);
   getPersonQuery(req.params.personId)
     .then((person) => {
       console.log(person, "ppppppppppppppppppppppperson");
