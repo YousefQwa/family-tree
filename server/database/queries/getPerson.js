50130e88306e5488a93bcbe9e8d5863b08283d70
YousefQwasmeh <151037@ppu.edu.ps>
2021-02-27 15:31:57 +0200

    console log

--- server/database/queries/getPerson.js
+++ server/database/queries/getPerson.js
@@ -1,6 +1,17 @@
 const dbConnection = require("../dbConnection");
 const getPerson = (id) => {
   console.log("111111111111111111111111111", id);
+  dbConnection
+    .query("select * from person where id = $1", [id])
+    .then((res) => {
+      console.log(res.rows, "resresres0000000000000000000000");
+      return res.rows;
+    })
+    .catch((err) => {
+      console.log(err, "7777777777777 eeeeerrrrrrrrr");
+
+      return err;
+    });
   return dbConnection
     .query("select * from person where id = $1", [id])
     .then((res) => {
